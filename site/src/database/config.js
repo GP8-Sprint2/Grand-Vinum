b1150ce9bbae3afbd7359290656f1056ed7370d1
MRESENDESILVA <mateus.rsilva@sptech.school>
2022-10-20 21:46:44 -0300

    UPDATE - Simulador financeiro (MENSAGEM) - API (LOGIN)

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "GrandVinum",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "Cl45896321",
 };
 
 function executar(instrucao) {
